bdc6789ecb7ce889effbf44d5c5c690790d08b31
yuchangfu <changfu.yu@autonavi.com>
2014-08-13 20:56:09 +0800

    add timer

--- src/com/ftc/themostboringman/MainActivity.java
+++ src/com/ftc/themostboringman/MainActivity.java
@@ -1,48 +1,126 @@
 package com.ftc.themostboringman;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ftc.themosrboringman.R;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends Activity {
-	private AdView adView;
+    private TextView tvTime;
+    private Button btnTouch;
+    private AdView adView;
+    private Timer touchTimer;
+    private TimerTask touchTask;
+    private final int MSG_SET_TEXT = 0;
+    private final int MSG_TOUCH_END = 1;
+    private long startTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	tvTime = (TextView)findViewById(R.id.tv_touch_time);
+	initAd();
+	initTouchButton();
+    }
+
+    private void initAd() {
+	adView = (AdView) findViewById(R.id.ad_view);
+	AdRequest adRequest = new AdRequest.Builder().build();
+	adView.loadAd(adRequest);
+    }
+
+    private Handler handler = new Handler() {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initAd();
-	}
+	public void handleMessage(Message msg) {
+	    switch (msg.what) {
+	    case MSG_SET_TEXT:
+		Long time= (Long) msg.obj;
+		String timeStr = getTimeFormat(startTime,time.longValue());
+		tvTime.setText(timeStr);
+		break;
+	    case MSG_TOUCH_END:
+		break;
+	    }
 
-	private void initAd() {
-		adView = (AdView)findViewById(R.id.ad_view);
-		// 启动一般性请求。
-		AdRequest adRequest = new AdRequest.Builder().build();
-
-		// 在adView中加载广告请求。
-		adView.loadAd(adRequest);
 	}
+    };
+    public String getTimeFormat(long startTime,long endTime){
+	return endTime-startTime+"";
+    }
 
-	@Override
-	public void onPause() {
-		adView.pause();
-		super.onPause();
-	}
+    private void initTimer() {
+	touchTimer = new Timer();
+	touchTask = new TimerTask() {
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		adView.resume();
-	}
+	    @Override
+	    public void run() {
+		Message msg = new Message();
+		msg.what = MSG_SET_TEXT;
+		msg.obj = System.currentTimeMillis();
+		handler.sendMessage(msg);
+	    }
 
-	@Override
-	public void onDestroy() {
-		adView.destroy();
-		super.onDestroy();
-	}
+	};
+    }
+
+    private void initTouchButton() {
+
+	btnTouch = (Button) findViewById(R.id.btn_touch);
+	btnTouch.setOnTouchListener(new OnTouchListener() {
+
+	    @Override
+	    public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+		    initTimer();
+		    startTime = System.currentTimeMillis();
+		    touchTimer.schedule(touchTask, 0, 100);
+		    break;
+		case MotionEvent.ACTION_OUTSIDE:
+		case MotionEvent.ACTION_CANCEL:
+		case MotionEvent.ACTION_UP:
+		    touchTimer.cancel();
+		    Message msg = new Message();
+		    msg.what = MSG_SET_TEXT;
+		    msg.obj = System.currentTimeMillis();
+		    handler.sendMessage(msg);
+		    break;
+		}
+		return false;
+	    }
+
+	});
+    }
+
+    @Override
+    public void onPause() {
+	adView.pause();
+	super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+	super.onResume();
+	adView.resume();
+    }
+
+    @Override
+    public void onDestroy() {
+	adView.destroy();
+	super.onDestroy();
+    }
 }
